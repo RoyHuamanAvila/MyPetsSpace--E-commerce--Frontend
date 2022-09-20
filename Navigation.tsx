5b9dbbf135ea43145d4ecb5e5b601d0bbfe61e05
Roy <rhavila789@gmail.com>
2022-09-20 15:47:46 -0500

    feat: Tab navigation (extra: Home page, Header Component, ScrollAds)

---
+++ Navigation.tsx
@@ -0,0 +1,38 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import Profile from "./screens/Profile";
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+const MyTabs = () => {
+    return (
+        <Tab.Navigator initialRouteName="Home" screenOptions={{
+            tabBarActiveTintColor: '#11a76a',
+        }}>
+            <Tab.Screen name="Home" component={Home} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="md-home-sharp" size={size} color={color} />
+                ),
+                headerShown: false
+            }} />
+            <Tab.Screen name="Profile" component={Profile} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons name="account" size={size} color={color} />
+                ),
+            }} />
+        </Tab.Navigator>
+    )
+}
+
+const Navigation = () => {
+    return (
+        <NavigationContainer>
+            <MyTabs />
+        </NavigationContainer>
+    )
+}
+
+export default Navigation;
---
+++ screens/Home.tsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header/Index";
+
+const Home = () => {
+    return (
+        <Header />
+    )
+}
+
+export default Home;
